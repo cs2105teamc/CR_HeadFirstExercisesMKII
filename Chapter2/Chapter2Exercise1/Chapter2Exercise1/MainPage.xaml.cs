9e70464b91cd2e3d45515e78bfd08f2bc0bc834d
rashadac <RASHADAC@tcd.ie>
2015-11-04 21:33:33 +0000

    Layout complete

--- Chapter2/Chapter2Exercise1/Chapter2Exercise1/MainPage.xaml.cs
+++ Chapter2/Chapter2Exercise1/Chapter2Exercise1/MainPage.xaml.cs
@@ -26,5 +26,25 @@ namespace Chapter2Exercise1
         {
             this.InitializeComponent();
         }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
